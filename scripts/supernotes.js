a0ec8067bc80726848f315e660a2782bb7977043
Brian Jones <brian@foamgarden.com>
2016-03-07 06:50:40 +0100

    Make supernote links icon-based

--- scripts/supernotes.js
+++ scripts/supernotes.js
@@ -5,13 +5,43 @@ function showSupernote(supernote, number) {
   }
 }
 
-function buildSupernoteLink(supernote, number) {
-  var label = '[' + supernote['type'] + ']';
+function supernoteLinkTitle(type, count, number) {
+  var label;
+  var plural = count > 1;
 
+  switch (type) {
+    case 'commentary':
+      label = (plural) ? 'Commentaries' : 'Commentary';
+      break;
+    case 'citation':
+      label = (plural) ? 'Citations' : 'Citation';
+      break;
+    case 'image':
+      label = (plural) ? 'Images' : 'Image';
+      break;
+    case 'video':
+      label = (plural) ? 'Videos' : 'Video';
+      break;
+    case 'map':
+      label = (plural) ? 'Maps' : 'Map';
+      break;
+    case 'link':
+      label = (plural) ? 'Links' : 'Link';
+      break;
+  }
+
+  return label + ' for paragraph ' + number;
+}
+
+function buildSupernoteLink(supernote, number) {
   link = $('<a>')
             .attr('href', '#paragraph-' + number)
             .attr('id', supernote['type'] + '-' + number + '-supernote')
-            .text(label);
+            .attr('title', 
+              supernoteLinkTitle(
+                supernote['type'], supernote['notes'].length, number))
+            .addClass('supernote-icon')
+            .addClass('icon-' + supernote['type']);
   $('#paragraph-' + number).append(link);
 
   (function(supernote, paragraph) {
